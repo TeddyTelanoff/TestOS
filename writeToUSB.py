d7aae6113418e1ab34be5a5506000d3da18e4c34
Treidex <treidexsemextetry@gmail.com>
2021-06-15 17:02:14 -0700

    more code!

--- writeToUSB.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_fd = os.open(r"boot.img", os.O_RDONLY | os.O_BINARY)
-data = os.read(file_fd, os.stat(r"boot.img").st_size)
-os.close(file_fd)
-
-usb_fd = os.open(r"F:\\", os.O_WRONLY | os.O_BINARY)
-os.write(usb_fd, data)
-os.close(usb_fd)
